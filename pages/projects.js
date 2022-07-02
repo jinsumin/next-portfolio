9a7d89a3d0cd72a654234d2f798b422bc2d1cdf0
jinsumin <suminjin@naver.com>
2022-07-03 00:32:26 +0900

    solve deploy error

--- pages/projects.js
+++ pages/projects.js
@@ -5,9 +5,9 @@ export default function Projects({ results }) {
     <>
       <section className="text-gray-600 body-font overflow-hidden">
         <div className="grid grid-cols-1 md:grid-cols-2 p-12 m-4 gap-8">
-          {results.map((aProject) => (
+          {/* {results.map((aProject) => (
             <ProjectItems key={aProject.id} data={aProject} />
-          ))}
+          ))} */}
         </div>
       </section>
     </>
