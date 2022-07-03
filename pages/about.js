4d1f46a1f39b6e18afc2bc5fa0d1c664c6615c64
jinsumin <suminjin@naver.com>
2022-07-04 03:04:06 +0900

    update main

--- pages/about.js
+++ pages/about.js
@@ -5,7 +5,7 @@ export default function About() {
         <div className="container px-5 py-24 mx-auto">
           <div className="flex flex-col text-center w-full mb-20">
             <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">
-              Master Cleanse Reliac Heirloom
+              About Me
             </h1>
             <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
               Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical
--- pages/main.js
+++ pages/main.js
@@ -6,16 +6,21 @@ export default function Main() {
     <>
       <section className="text-gray-600 body-font">
         <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
-        <div className="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
+          <div className="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
             <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
-              <TypingEffect text={'안녕하세요! 진수민의 <br> 포트폴리오 페이지 입니다.'}/>
+              <TypingEffect
+                text={"안녕하세요! 진수민의 <br> 포트폴리오 페이지 입니다."}
+              />
               <br className="hidden lg:inline-block" />
             </h1>
             <p className="mb-8 leading-relaxed">
-              Copper mug try-hard pitchfork pour-over freegan heirloom neutra
-              air plant cold-pressed tacos poke beard tote bag. Heirloom echo
-              park mlkshk tote bag selvage hot chicken authentic tumeric
-              truffaut hexagon try-hard chambray.
+              저는 새로운 지식과 기술을 배우는 것을 좋아하며 경험을 공유하고{" "}
+              <br />
+              함께 성장하는 가치를 중요시합니다. 혼자만의 역량만이 아니라 내
+              주변, <br />
+              그리고 함께하는 팀의 성장을 도울 수 있는 개발자가 되기 위해
+              노력하고 있습니다. <br />
+              
             </p>
             <div className="flex justify-center">
               <button className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
