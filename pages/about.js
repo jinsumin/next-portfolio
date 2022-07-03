87076e11e3482c279c95657ccc61377253e25bc3
jinsumin <suminjin@naver.com>
2022-07-04 03:55:24 +0900

    update about me

--- pages/about.js
+++ pages/about.js
@@ -1,3 +1,6 @@
+import Animation from "../components/animation";
+import GitHubJson from "/public/github03.json";
+
 export default function About() {
   return (
     <>
@@ -8,31 +11,37 @@ export default function About() {
               About Me
             </h1>
             <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
-              Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical
-              gentrify, subway tile poke farm-to-table. Franzen you probably
-              heard of them man bun deep jianbing selfies heirloom prism
-              food truck ugh squid celiac humblebrag.
+              C와 Java 언어를 통해 프로그래밍을 시작하였고 CS, 운영체제,
+              자료구조와 알고리즘 <br />
+              OOP와 클린코드 관련 책을 읽고 그룹 스터디를 통해 학습해왔습니다.{" "}
+              <br />
+              개발자는 비즈니스 도메인에 대한 지식과 함께 좋은 퍼포먼스와
+              가독성이 좋고 확장에 유연한 <br />
+              코드를 작성해야 한다고 생각하며 이를 위해 계속 고민하고 있습니다.
             </p>
           </div>
           <div className="flex flex-wrap -m-4 text-center">
             <div className="p-4 md:w-1/4 sm:w-1/2 w-full">
               <div className="border-2 border-gray-200 px-4 py-6 rounded-lg">
-                <svg
-                  fill="none"
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  className="text-indigo-500 w-12 h-12 mb-3 inline-block"
-                  viewBox="0 0 24 24"
-                >
-                  <path d="M8 17l4 4 4-4m-4-5v9"></path>
-                  <path d="M20.88 18.09A5 5 0 0018 9h-1.26A8 8 0 103 16.29"></path>
-                </svg>
+                <div className="text-indigo-500 w-12 h-12 mb-3 inline-block">
+                  <svg
+                    fill="none"
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    className="text-indigo-500 w-12 h-12 mb-3 inline-block"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M17 21v-2a4 4 0 00-4-4H5a4 4 0 00-4 4v2"></path>
+                    <circle cx="9" cy="7" r="4"></circle>
+                    <path d="M23 21v-2a4 4 0 00-3-3.87m-4-12a4 4 0 010 7.75"></path>
+                  </svg>
+                </div>
                 <h2 className="title-font font-medium text-3xl text-gray-900">
-                  2.7K
+                  깃허브
                 </h2>
-                <p className="leading-relaxed">Downloads</p>
+                <p className="leading-relaxed">github.com/jinsumin</p>
               </div>
             </div>
             <div className="p-4 md:w-1/4 sm:w-1/2 w-full">
@@ -51,9 +60,9 @@ export default function About() {
                   <path d="M23 21v-2a4 4 0 00-3-3.87m-4-12a4 4 0 010 7.75"></path>
                 </svg>
                 <h2 className="title-font font-medium text-3xl text-gray-900">
-                  1.3K
+                  블로그
                 </h2>
-                <p className="leading-relaxed">Users</p>
+                <p className="leading-relaxed">devlog-soomin.tistory.com</p>
               </div>
             </div>
             <div className="p-4 md:w-1/4 sm:w-1/2 w-full">
@@ -71,9 +80,9 @@ export default function About() {
                   <path d="M21 19a2 2 0 01-2 2h-1a2 2 0 01-2-2v-3a2 2 0 012-2h3zM3 19a2 2 0 002 2h1a2 2 0 002-2v-3a2 2 0 00-2-2H3z"></path>
                 </svg>
                 <h2 className="title-font font-medium text-3xl text-gray-900">
-                  74
+                  북 스터디
                 </h2>
-                <p className="leading-relaxed">Files</p>
+                <p className="leading-relaxed">github.com/dev-book-readers</p>
               </div>
             </div>
             <div className="p-4 md:w-1/4 sm:w-1/2 w-full">
@@ -90,9 +99,9 @@ export default function About() {
                   <path d="M12 22s8-4 8-10V5l-8-3-8 3v7c0 6 8 10 8 10z"></path>
                 </svg>
                 <h2 className="title-font font-medium text-3xl text-gray-900">
-                  46
+                  알고리즘
                 </h2>
-                <p className="leading-relaxed">Places</p>
+                <p className="leading-relaxed">acmicpc.net/user/smjin031</p>
               </div>
             </div>
           </div>
