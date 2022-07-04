b379702f3222286eb7280a655988bad289a9cc18
jinsumin <suminjin@naver.com>
2022-07-05 02:02:56 +0900

    apply sticky header

--- components/header.js
+++ components/header.js
@@ -4,7 +4,7 @@ import Animation from "./animation";
 export default function Header() {
   return (
     <>
-      <header className="text-gray-600 body-font shadow-2xl">
+      <header className="sticky top-0 z-50 text-gray-600 body-font shadow-2xl">
         <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
           <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
             <Animation width={30} height={30}/>
