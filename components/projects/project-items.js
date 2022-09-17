e661a4955a861a6aa3ffac2a8a19574c6cdfa3af
jinsumin <suminjin@naver.com>
2022-09-17 22:14:02 +0900

    flex-wrap

--- components/projects/project-items.js
+++ components/projects/project-items.js
@@ -57,7 +57,7 @@ export default function ProjectItems({ data }) {
                 프로젝트 기간 : {start} ~ {end} ({calculatePeriod(start, end)}
                 일)
               </p>
-              <div className="flex items-start mt-2">
+              <div className="flex flex-wrap items-start mt-2">
                 {tags.map((aTag) => (
                   <h1
                     className="px-2 py-1 mr-2 rounded-md bg-sky-200 dark:bg-sky-700 w-30"
