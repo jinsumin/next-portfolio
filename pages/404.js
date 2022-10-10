56fc5fee77a853d88feeb8a10b2caa7796dea0b3
jinsumin <suminjin@naver.com>
2022-10-10 12:59:32 +0900

    update

--- pages/404.js
+++ pages/404.js
@@ -1,7 +1,7 @@
 export default function PageNotFound() {
   return (
     <div>
-      <h1>페이지를 찾을 수 없습니다.</h1>
+      <h1>페이지를 찾을 수 없습니다. url을 정확하게 입력하였는지 확인해주세요</h1>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,6 @@ module.exports = {
     extend: {
       fontFamily: {
         dohyeon: "DoHyeon, cursive",
-        notoSansKR: "NotoSans",
       },
     },
   },
