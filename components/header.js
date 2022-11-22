5cf95ad096384b599114dd39b6680b7ca45c630b
jinsumin <suminjin@naver.com>
2022-11-22 17:47:59 +0900

    inprogress : header menu

--- components/header.js
+++ components/header.js
@@ -1,6 +1,7 @@
 import DarkModeToggleButton from "./dark-mode-toggle-button";
 import Animation from "./animation";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import classNames from "classnames";
 
 export default function Header() {
   useEffect(() => {
@@ -13,6 +14,8 @@ export default function Header() {
       });
     });
   });
+
+  const [menuToggle, setMenuToggle] = useState(false);
 
   return (
     <>
@@ -51,31 +54,50 @@ export default function Header() {
           <DarkModeToggleButton />
         </div>
       </header>
-      <header className="md:hidden flex sticky top-0 z-50 text-gray-600 body-font shadow-2xl bg-gray-500 bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-10">
-        <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+      <header className="md:hidden sticky top-0 z-50 text-gray-600 body-font shadow-2xl bg-gray-500 bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-10">
+        <div className="container mx-auto flex flex-wrap flex-col md:flex-row items-center pt-4">
           <a
             href="#main-section"
-            className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0"
+            className="flex order-first lg:order-none items-center justify-center lg:items-center lg:justify-center title-font font-medium text-gray-900 mb-4 md:mb-0"
           >
             <Animation width={30} height={30} />
             <span className="ml-3 text-xl">진수민의 포트폴리오</span>
           </a>
-          <button>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M4 6h16M4 12h16M4 18h16"
-              />
-            </svg>
-          </button>
+          {/* <div className="lg:w-2/5 inline-flex lg:justify-end ml-5 lg:ml-0">
+            <button onClick={() => setMenuToggle(!menuToggle)} className="inline-flex items-center">
+              {menuToggle ? (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                </svg>
+              ) : (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M4 6h16M4 12h16M4 18h16"
+                  />
+                </svg>
+              )}
+            </button>
+          </div> */}
         </div>
       </header>
     </>
