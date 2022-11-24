b7a0b239bd7b5d4f0e27ec4972c8b34dea6f2422
jinsumin <suminjin@naver.com>
2022-11-24 22:54:41 +0900

    margin

--- pages/skills.js
+++ pages/skills.js
@@ -13,9 +13,9 @@ export default function Skills() {
             </p>
           </div>
 
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 grid-rows-1 gap-20 text-center">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 grid-rows-1 gap-20 text-center mx-10">
             <div className="row-span-1 space-y-16">
-              <div className="border rounded-lg bg-white shadow-xxl transition duration-300 transform hover:scale-105 shadow-xl hover:shadow-2xl dark:hover:shadow-gray-400/40 hover:text-blue-600">
+              <div className="border mx-4 rounded-lg bg-white shadow-xxl transition duration-300 transform hover:scale-105 shadow-xl hover:shadow-2xl dark:hover:shadow-gray-400/40 hover:text-blue-600">
                 <div className="text-3xl font-bold text-cyan-600 border-b-2 border-cyan-600 m-2 mb-10">
                   Frontend
                 </div>
@@ -26,7 +26,7 @@ export default function Skills() {
                   height={900}
                 />
               </div>
-              <div className="border rounded-lg bg-white shadow-xxl transition duration-300 transform hover:scale-105 shadow-xl hover:shadow-2xl dark:hover:shadow-gray-400/40 hover:text-blue-600">
+              <div className="border mx-4 rounded-lg bg-white shadow-xxl transition duration-300 transform hover:scale-105 shadow-xl hover:shadow-2xl dark:hover:shadow-gray-400/40 hover:text-blue-600">
                 <div className="text-3xl font-bold text-cyan-600 border-b-2 border-cyan-600 m-2 mb-10">
                   Mobile
                 </div>
@@ -40,7 +40,7 @@ export default function Skills() {
             </div>
 
             <div className="row-span-1 space-y-16">
-              <div className="border rounded-lg bg-white shadow-xxl transition duration-300 transform hover:scale-105 shadow-xl hover:shadow-2xl dark:hover:shadow-gray-400/40 hover:text-blue-600">
+              <div className="border mx-4 rounded-lg bg-white shadow-xxl transition duration-300 transform hover:scale-105 shadow-xl hover:shadow-2xl dark:hover:shadow-gray-400/40 hover:text-blue-600">
                 <div className="text-3xl font-bold text-cyan-600 border-b-2 border-cyan-600 m-2 mb-10">
                   Backend
                 </div>
@@ -52,7 +52,7 @@ export default function Skills() {
                 />
               </div>
 
-              <div className="border rounded-lg bg-white shadow-xxl transition duration-300 transform hover:scale-105 shadow-xl hover:shadow-2xl dark:hover:shadow-gray-400/40 hover:text-blue-600">
+              <div className="border mx-4 rounded-lg bg-white shadow-xxl transition duration-300 transform hover:scale-105 shadow-xl hover:shadow-2xl dark:hover:shadow-gray-400/40 hover:text-blue-600">
                 <div className="text-3xl font-bold text-cyan-600 border-b-2 border-cyan-600 m-2 mb-10">
                   Deployment
                 </div>
@@ -78,7 +78,7 @@ export default function Skills() {
                 />
               </div>
 
-              <div className="border rounded-lg bg-white shadow-xxl transition duration-300 transform hover:scale-105 shadow-xl hover:shadow-2xl dark:hover:shadow-gray-400/40 hover:text-blue-600">
+              <div className="border mx-4 rounded-lg bg-white shadow-xxl transition duration-300 transform hover:scale-105 shadow-xl hover:shadow-2xl dark:hover:shadow-gray-400/40 hover:text-blue-600">
                 <div className="text-3xl font-bold text-cyan-600 border-b-2 border-cyan-600 m-2 mb-10">
                   Certification
                 </div>
