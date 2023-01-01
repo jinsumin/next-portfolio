17ae0d2579468b1e261fb4667ca1764c04f8c992
jinsumin <suminjin@naver.com>
2023-01-01 15:43:08 +0900

    adjust google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,28 @@
 import "../styles/globals.css";
 import { ThemeProvider } from "next-themes";
 import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import Script from "next/script";
+import * as gtag from "../lib/gtag";
 
 function MyApp({ Component, pageProps }) {
   const [showChild, setShowChild] = useState(false);
   useEffect(() => {
     setShowChild(true);
   }, []);
+
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    router.events.on("hashChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+      router.events.off("hashChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
 
   if (!showChild) {
     return null;
@@ -17,6 +33,25 @@ function MyApp({ Component, pageProps }) {
   } else {
     return (
       <ThemeProvider attribute="class">
+        {/* Global Site Tag (gtag.js) - Google Analytics */}
+        <Script
+          strategy="afterInteractive"
+          src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+        />
+        <Script
+          id="gtag-init"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+          }}
+        />
         <Component {...pageProps} />
       </ThemeProvider>
     );
