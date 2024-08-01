ff4b327bd479df5ab1e5dd4308604c206b634e5f
진수민 <jimmy@jinsumin-ui-MacBookPro.local>
2024-08-01 15:50:24 +0900

    experience update

--- pages/experience.js
+++ pages/experience.js
@@ -4,7 +4,8 @@ import image02 from "/public/idea.png";
 import image03 from "/public/intern.png";
 import image04 from "/public/developer.png";
 import image05 from "/public/rising.png";
-import image06 from "/public/fintech-removebg-preview.png";
+import image06 from "/public/sangsangin.png";
+import image07 from "/public/fintech-removebg-preview.png";
 
 export default function Experience() {
   return (
@@ -12,7 +13,7 @@ export default function Experience() {
       <section className="text-gray-600 body-font">
         <div className="flex flex-col text-center w-full mt-20">
           <h1 className="sm:text-3xl text-2xl font-medium title-font text-gray-900">
-            Experience
+            Education | Experience
           </h1>
         </div>
         <div className="container px-5 py-24 mx-auto flex flex-wrap">
@@ -122,7 +123,7 @@ export default function Experience() {
               </div>
             </div>
           </div>
-          {/* 6. 금융투자협회 핀테크아카데미 */}
+          {/* 6. 상상인증권 */}
           <div className="flex relative pt-10 pb-10 sm:items-center md:w-2/3 mx-auto">
             <div className="invisible lg:visible xl:visible 2xl:visible">
               <div className="h-full w-6 absolute inset-0 flex items-center justify-center">
@@ -134,7 +135,30 @@ export default function Experience() {
             </div>
             <div className="flex-grow md:pl-8 pl-6 flex sm:items-center items-start flex-col sm:flex-row">
               <div className="flex-shrink-0 w-24 h-24 bg-indigo-100 text-cyan-500 rounded-full inline-flex items-center justify-center">
-                <Image src={image06} alt="school" width={55} height={55}/>
+                <Image src={image06} alt="school"/>
+              </div>
+              <div className="flex-grow sm:pl-6 mt-6 sm:mt-0">
+                <h2 className="font-medium title-font text-gray-900 mb-1 text-xl">
+                  상상인증권 (재직중)
+                  <p className="pt-1 text-base">IT개발팀 / 2023. 01. 16 ~ 2024. 01. 15 / MTS 개발자 </p>
+                  <p className="text-base">CX기획팀 / 2024. 01. 16 ~ present / 프론트엔드 개발자 </p>
+                </h2>
+              </div>
+            </div>
+          </div>
+          {/* 6. 금융투자협회 핀테크아카데미 */}
+          <div className="flex relative pt-10 pb-10 sm:items-center md:w-2/3 mx-auto">
+            <div className="invisible lg:visible xl:visible 2xl:visible">
+              <div className="h-full w-6 absolute inset-0 flex items-center justify-center">
+                <div className="h-full w-1 bg-gray-200 pointer-events-none"></div>
+              </div>
+              <div className="flex-shrink-0 w-6 h-6 rounded-full mt-10 sm:mt-0 inline-flex items-center justify-center bg-cyan-500 text-white relative z-10 title-font font-medium text-sm">
+                7
+              </div>
+            </div>
+            <div className="flex-grow md:pl-8 pl-6 flex sm:items-center items-start flex-col sm:flex-row">
+              <div className="flex-shrink-0 w-24 h-24 bg-indigo-100 text-cyan-500 rounded-full inline-flex items-center justify-center">
+                <Image src={image07} alt="school" width={55} height={55}/>
               </div>
               <div className="flex-grow sm:pl-6 mt-6 sm:mt-0">
                 <h2 className="font-medium title-font text-gray-900 mb-1 text-xl">
