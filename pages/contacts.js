7de59346c940c08aeff4272535baa353635d8a8e
진수민 <jimmy@jinsumin-ui-MacBookPro.local>
2024-09-15 14:38:47 +0900

    update

--- pages/contacts.js
+++ pages/contacts.js
@@ -37,7 +37,7 @@ export default function Contacts() {
       <section className="text-gray-600 body-font relative">
         <div className="container px-5 py-24 mx-auto">
           <div className="flex flex-col text-center w-full mb-12">
-            <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900 mb-10">
+            <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">
               Contact Me
             </h1>
             <div className="xl:w-1/2 lg:w-3/4 w-full mx-auto text-center">
